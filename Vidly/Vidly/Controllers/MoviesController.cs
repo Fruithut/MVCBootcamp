653d3451a81e5a82e7dabfd9f310575781be9b2f
Fruithut <fruithut@tutanota.com>
2019-03-20 03:22:56 +0100

    The beginnings of my first MVC project

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie {Id = 1, Name = "Cats in space"},
+                new Movie {Id = 1, Name = "A weird occurence"}
+            };
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Top Gun" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
